adb872a975c48beda1fce289fc7fa533162ad52a
Rob Rossmiller <rrossmiller3@gmail.com>
2022-11-12 13:59:44 -0500

    parallel with wait group

--- golang/gogrep/main.go
+++ golang/gogrep/main.go
@@ -6,100 +6,102 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 	"unicode/utf8"
 
 	"github.com/fatih/color"
 )
 
 var green = color.New(color.FgRed).SprintFunc()
+var noCase *bool
 
 func main() {
-	// start := time.Now()
-	parallel := flag.Bool("p", false, "search files in parallel")
-	noCase := flag.Bool("i", false, "case insensitve")
+	// parallel := flag.Bool("p", false, "search files in parallel")
+	noCase = flag.Bool("i", false, "case insensitve")
 	// recursive := flag.Bool("r", false, "recursive")
+	// regex := flag.Bool("r", false, "regex")
 	flag.Parse()
 
 	args := flag.Args()
-	pattern, file := args[0], args[1]
+	pattern, fileNames := args[0], args[1:]
 	if *noCase {
 		pattern = strings.ToLower(pattern)
 	}
-	// regex
 
-	if _, e := os.Stat(file); errors.Is(e, os.ErrNotExist) {
-		// panic(e)
-		fmt.Printf("%q does not exist\n", file)
-		os.Exit(1)
+	var outputSb strings.Builder
+	var wg sync.WaitGroup
+	wg.Add(len(fileNames))
+
+	// for every file
+	for _, fileName := range fileNames {
+		// check if the file exists
+		if _, e := os.Stat(fileName); errors.Is(e, os.ErrNotExist) {
+			fmt.Printf("%q does not exist\n", fileName)
+			os.Exit(1)
+		}
+
+		multiFile := len(fileNames) > 1
+		go searchFile(fileName, pattern, multiFile, &outputSb, &wg) // fixme write files together
 	}
+	wg.Wait()
+	// print the output
+	fmt.Println(outputSb.String())
+}
 
+func searchFile(fileName, pattern string, multiFile bool, outputSb *strings.Builder, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// read the file
 	var fileBytes []byte
-	fileBytes, e := os.ReadFile(file)
+	fileBytes, e := os.ReadFile(fileName) //todo stream file intsead of opening the whole thing
 	check(e)
 	fileContents := string(fileBytes)
 
-	// check the file contents for the input string
-	// TODO support multiple instance in the same line (instead of seeing if the line has it,
-	// slide over each token and add to map if found (key = line num, val = str))
+	// check the file's contents for the pattern
 	patternLength := utf8.RuneCountInString(pattern)
 	lines := strings.Split(fileContents, "\n")
-	var sb strings.Builder
 	var lineSb strings.Builder
-	if *parallel {
-		fmt.Println("not implemented yet")
-		// 	fmt.Println("running parallel")
-		// 	var wg sync.WaitGroup
-		// 	wg.Add(len(lines))
-		// 	for i, line := range lines {
-		// 		go findParallel(i, patternLength, line, pattern, &wg)
-		// 	}
-		// 	wg.Wait()
-	} else {
-		for i, line := range lines {
-			lineHasString := false
-			lineCheck := strings.Clone(line)
-			// if ignore case, make everything lowercase
-			if *noCase {
-				lineCheck = strings.ToLower(lineCheck)
-			}
-			// for every word on the line
-			lineSplit := strings.Split(line, " ")
-			for j, word := range strings.Split(lineCheck, " ") {
-				// if the word contains the pattern
-				if strings.Contains(word, pattern) {
-					lineHasString = true
-					idx := strings.Index(word, pattern)                           // start index of the pattern in the word
-					pre := string(lineSplit[j][:idx])                             // string before the pattern
-					match := green(string(lineSplit[j][idx : idx+patternLength])) //pattern in the string
-					post := string(lineSplit[j][idx+patternLength:])              // string after the pattern
-					lineSplit[j] = pre + match + post
-				}
-				lineSb.WriteString(lineSplit[j] + " ")
-			}
-			if lineHasString {
-				s := fmt.Sprintf("%d: %s\n", i+1, lineSb.String())
-				sb.WriteString(s)
-			}
-			lineSb.Reset()
-		}
-	}
-	fmt.Println(sb.String())
-	// fmt.Printf("elapsed: %v seconds\n", time.Since(start))
-}
+	for i, line := range lines {
+		lineHasPattern := false           //default: the line doesn't contain pattern
+		lineMutStr := strings.Clone(line) // make a clone of the line so the output maintains the input's capitalization
 
-// func findParallel(i, patternLength int, line, pattern string, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	if strings.Contains(line, pattern) {
-// 		idx := strings.Index(line, pattern)
-// 		pre := string(line[:idx])
-// 		post := string(line[idx+patternLength:])
-// 		fmt.Printf("%d: %s%s%s\n", i+1, pre, green((pattern)), post)
-// 	}
-// }
+		// if ignore case, make everything lowercase
+		if *noCase {
+			lineMutStr = strings.ToLower(lineMutStr)
+		}
+
+		// for every word on the line
+		lineSplit := strings.Split(line, " ")
+		for j, word := range strings.Split(lineMutStr, " ") {
+			// if the word contains the pattern
+			if strings.Contains(word, pattern) {
+				lineHasPattern = true                                         // the pattern is found in the line
+				idx := strings.Index(word, pattern)                           // start index of the pattern in the word
+				pre := string(lineSplit[j][:idx])                             // string before the pattern
+				match := green(string(lineSplit[j][idx : idx+patternLength])) // hightlight the pattern in the string
+				post := string(lineSplit[j][idx+patternLength:])              // string after the pattern
+				lineSplit[j] = pre + match + post                             // overwrite the text with the highlighted pattern
+			}
+			lineSb.WriteString(lineSplit[j] + " ") // write the word to the line string builder
+		}
+
+		// if the line has the pattern, write the line and line number to the output
+		if lineHasPattern {
+			var s string
+			if multiFile {
+				s = fmt.Sprintf("%d:%s: %s\n", i+1, fileName, lineSb.String())
+			} else {
+				s = fmt.Sprintf("%d: %s\n", i+1, lineSb.String())
+			}
+			outputSb.WriteString(s)
+		}
+		lineSb.Reset() // reset the line string builder for the next line
+	}
+}
 
 func check(e error) {
 	if e != nil {
 		panic(e)
-		// log.Fatal("something went wrong reading the file")
+		// log.Fatal("something went wrong reading the fileNames")
 	}
 }
