3553ac33242fa81c25abe0f0e063dd472a5ba9e3
Rob Rossmiller <rrossmiller3@gmail.com>
2022-11-11 23:48:41 -0500

    preserve output case

--- golang/gogrep/main.go
+++ golang/gogrep/main.go
@@ -17,16 +17,15 @@ func main() {
 	// start := time.Now()
 	parallel := flag.Bool("p", false, "search files in parallel")
 	noCase := flag.Bool("i", false, "case insensitve")
+	// recursive := flag.Bool("r", false, "recursive")
 	flag.Parse()
+
 	args := flag.Args()
 	pattern, file := args[0], args[1]
 	if *noCase {
 		pattern = strings.ToLower(pattern)
 	}
-	// -i, --ignore-case
 	// regex
-	// recursive
-	// line number
 
 	if _, e := os.Stat(file); errors.Is(e, os.ErrNotExist) {
 		// panic(e)
@@ -45,6 +44,7 @@ func main() {
 	patternLength := utf8.RuneCountInString(pattern)
 	lines := strings.Split(fileContents, "\n")
 	var sb strings.Builder
+	var lineSb strings.Builder
 	if *parallel {
 		fmt.Println("not implemented yet")
 		// 	fmt.Println("running parallel")
@@ -56,18 +56,31 @@ func main() {
 		// 	wg.Wait()
 	} else {
 		for i, line := range lines {
+			lineHasString := false
+			lineCheck := strings.Clone(line)
+			// if ignore case, make everything lowercase
 			if *noCase {
-				line = strings.ToLower(line)
+				lineCheck = strings.ToLower(lineCheck)
 			}
-			// if cnt := strings.Count(line, pattern); strings.Contains(line, pattern) {
-			if strings.Contains(line, pattern) {
-				idx := strings.Index(line, pattern)
-				pre := string(lines[i][:idx])
-				match := green(string(lines[i][idx : idx+patternLength]))
-				post := string(lines[i][idx+patternLength:])
-				s := fmt.Sprintf("%d: %s%s%s\n", i+1, pre, match, post)
+			// for every word on the line
+			lineSplit := strings.Split(line, " ")
+			for j, word := range strings.Split(lineCheck, " ") {
+				// if the word contains the pattern
+				if strings.Contains(word, pattern) {
+					lineHasString = true
+					idx := strings.Index(word, pattern)                           // start index of the pattern in the word
+					pre := string(lineSplit[j][:idx])                             // string before the pattern
+					match := green(string(lineSplit[j][idx : idx+patternLength])) //pattern in the string
+					post := string(lineSplit[j][idx+patternLength:])              // string after the pattern
+					lineSplit[j] = pre + match + post
+				}
+				lineSb.WriteString(lineSplit[j] + " ")
+			}
+			if lineHasString {
+				s := fmt.Sprintf("%d: %s\n", i+1, lineSb.String())
 				sb.WriteString(s)
 			}
+			lineSb.Reset()
 		}
 	}
 	fmt.Println(sb.String())
