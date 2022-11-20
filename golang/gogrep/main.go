8289233afa232f05e932d537a4c3db3a1a854ed0
Rob Rossmiller <rrossmiller3@gmail.com>
2022-11-20 12:44:05 -0500

    perf test. files par, lines par

--- golang/gogrep/main.go
+++ golang/gogrep/main.go
@@ -15,6 +15,7 @@ import (
 var red = color.New(color.FgRed).SprintFunc()
 var blue = color.New(color.FgBlue).SprintFunc()
 var noCase *bool
+var multiFile bool
 
 func main() {
 	noCase = flag.Bool("i", false, "case insensitve")
@@ -39,8 +40,8 @@ func main() {
 			os.Exit(1)
 		}
 
-		multiFile := len(fileNames) > 1
-		go searchFile(fileName, pattern, multiFile, output, i, &wg)
+		multiFile = len(fileNames) > 1
+		go searchFilePar(fileName, pattern, multiFile, output, i, &wg)
 	}
 	wg.Wait()
 
@@ -99,8 +100,74 @@ func searchFile(fileName, pattern string, multiFile bool, output []string, outId
 		}
 		lineSb.Reset() // reset the line string builder for the next line
 	}
+
 }
 
+// search the files in parallel and lines in parallel
+func searchFilePar(fileName, pattern string, multiFile bool, output []string, outIdx int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// read the file
+	var fileBytes []byte
+	fileBytes, e := os.ReadFile(fileName) //todo stream file intsead of opening the whole thing
+	check(e)
+	fileContents := string(fileBytes)
+
+	// check the file's contents for the pattern
+	patternLength := utf8.RuneCountInString(pattern)
+	lines := strings.Split(fileContents, "\n")
+	linesCh := make(chan string)
+	defer close(linesCh)
+	numWorkers := 8
+
+	for i := 0; i < numWorkers; i++ {
+		go searchLine(linesCh, pattern, fileContents, output, outIdx, i, patternLength)
+	}
+	for _, v := range lines {
+		linesCh <- v
+	}
+
+}
+
+func searchLine(lineCh chan string, pattern, fileName string, output []string, outIdx, i, patternLength int) {
+	var lineSb strings.Builder
+	for line := range lineCh {
+		lineHasPattern := false           //default: the line doesn't contain pattern
+		lineMutStr := strings.Clone(line) // make a clone of the line so the output maintains the input's capitalization
+
+		// if ignore case, make everything lowercase
+		if *noCase {
+			lineMutStr = strings.ToLower(lineMutStr)
+		}
+
+		// for every word on the line
+		lineSplit := strings.Split(line, " ")
+		for j, word := range strings.Split(lineMutStr, " ") {
+			// if the word contains the pattern
+			if strings.Contains(word, pattern) {
+				lineHasPattern = true                                       // the pattern is found in the line
+				idx := strings.Index(word, pattern)                         // start index of the pattern in the word
+				pre := string(lineSplit[j][:idx])                           // string before the pattern
+				match := red(string(lineSplit[j][idx : idx+patternLength])) // hightlight the pattern in the string
+				post := string(lineSplit[j][idx+patternLength:])            // string after the pattern
+				lineSplit[j] = pre + match + post                           // overwrite the text with the highlighted pattern
+			}
+			lineSb.WriteString(lineSplit[j] + " ") // write the word to the line string builder
+		}
+
+		// if the line has the pattern, write the line and line number to the output
+		if lineHasPattern {
+			var s string
+			if multiFile {
+				s = fmt.Sprintf("%s:%s: %s\n", blue(i+1), blue(fileName), lineSb.String())
+			} else {
+				s = fmt.Sprintf("%s: %s\n", blue(i+1), lineSb.String())
+			}
+			output[outIdx] += s
+		}
+		lineSb.Reset()
+	}
+}
 func check(e error) {
 	if e != nil {
 		panic(e)
--- golang/time/main.go
+++ golang/time/main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 	"time"
 )
 
 func main() {
-	start := time.Now()
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
-	out, err := cmd.Output()
+	perf := flag.Bool("p", false, "perf test")
+	flag.Parse()
+	args := flag.Args()
+	if *perf {
+		perfTest(1000, args)
+	} else {
+		start := time.Now()
+		cmd := exec.Command(args[0], args[1:]...)
+		out, err := cmd.Output()
 
-	if err != nil {
-		// panic(err)
-		log.Fatal(err)
+		if err != nil {
+			// panic(err)
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+		fmt.Printf("elapsed: %v\n", time.Since(start))
 	}
-	fmt.Println(string(out))
-	fmt.Printf("elapsed: %v\n", time.Since(start))
+}
+
+func perfTest(n int, args []string) {
+	times := make([]time.Duration, n)
+
+	for i := 0; i < n; i++ {
+		start := time.Now()
+		cmd := exec.Command(args[0], args[1:]...)
+		_, err := cmd.Output()
+
+		if err != nil {
+			// panic(err)
+			log.Fatal(err)
+		}
+		times = append(times, time.Since(start))
+	}
+
+	t := AvgTime(times)
+	fmt.Printf("avg elapsed over %d tests: %f ms\n", n, t)
+}
+
+func AvgTime(times []time.Duration) float64 {
+	ttl := len(times)
+	sum := float64(0)
+	for i := 0; i < ttl; i++ {
+		sum += float64(times[i].Milliseconds())
+	}
+	return sum / (float64(ttl))
 }
