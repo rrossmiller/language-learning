65848517c995f07a91077ae694e7703f43069e2d
Rossmiller, Robert <robert.rossmiller@optum.com>
2022-12-21 11:16:18 -0500

    graph stuff

--- js/no-framework/tailwind.config.js
+++ js/no-framework/tailwind.config.js
@@ -1,9 +1,19 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "dsy",
+    darkTheme: "dark",
+  }
 }
