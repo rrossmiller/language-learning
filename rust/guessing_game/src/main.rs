4fe209bb137e4bb7dd8ef9118186acdcc315e7d3
Rob Rossmiller <rrossmiller3@gmail.com>
2023-03-29 00:14:31 -0400

    binary search guessing

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,20 +1,61 @@
 use rand::Rng;
-use std::io;
+use std::cmp::Ordering;
 
-// guessing game from the book
 fn main() {
+    let mut max = 100;
     println!("\nGuess the number");
-    let secret = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is {}", secret);
+    let secret = rand::thread_rng().gen_range(1..=max);
+    println!("secret number: {}", secret);
+    let mut guess = 50;
+    let mut min = 0;
+    for _ in 1..15 {
+        println!("guess: {} | {}-{}", guess, min, max);
 
-    println!("Enter your guess: ");
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read the line");
-
-    println!("\nYou guessed {guess}")
-    // !bookmark
-    // https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html#comparing-the-guess-to-the-secret-number
+        match guess.cmp(&secret) {
+            Ordering::Equal => {
+                println!("Nice 👍");
+                break;
+            }
+            Ordering::Less => {
+                // guess between max and min
+                min = guess;
+                guess = (max + min) / 2;
+            }
+            Ordering::Greater => {
+                max = guess;
+                guess = (max + min) / 2;
+            }
+        }
+    }
 }
+// // guessing game from the book
+// fn main() {
+//     println!("\nGuess the number");
+//     let secret = rand::thread_rng().gen_range(1..=100);
+//     // println!("The secret number is {}", secret);
+//     loop {
+//         println!("Enter your guess: ");
+//         let mut guess = String::new();
+
+//         io::stdin()
+//             .read_line(&mut guess)
+//             .expect("failed to read the line");
+//         let guess: u32 = match guess.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => {
+//                 println!("enter a number");
+//                 continue;
+//             }
+//         };
+//         println!("\nYou guessed {guess}");
+
+//         match guess.cmp(&secret) {
+//             Ordering::Equal => {
+//                 println!("Nice 👍");
+//                 break;
+//             }
+//             Ordering::Less => println!("too small"),
+//             Ordering::Greater => println!("too big"),
+//         }
+//     }
+// }
